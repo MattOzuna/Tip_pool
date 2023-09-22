8ed1ff6c4b2dd767bbf4397b142be1665fa29e43
Matt Ozuna <m.ozuna22@gmail.com>
2023-09-22 15:39:38 -0400

    completed servers.js tests and created helpers test

--- servers.test.js
+++ servers.test.js
@@ -10,9 +10,29 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(Object.keys(allServers).length).toEqual(1);
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
+  
+  it('should not add a new server to allServers when serverNameInput is ""', function(){
+    serverNameInput.value = '';
 
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(0);
+  });
+
+  it('should add serverNameInput and tipAverage to the serverTBody element', function(){
+    submitServerInfo();
+    updateServerTable();
+
+    let list = document.getElementById('server1').childNodes;
+
+    expect(list.length).toEqual(2);
+    expect(list[0].innerText).toEqual('Alice');
+    expect(list[1].innerText).toEqual('$0.00');
+  });
+  
   afterEach(function() {
     // teardown logic
     allServers = {}
+    serverId = 0
   });
 });
